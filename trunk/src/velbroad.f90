5e2837c0e3ed7caa1584d9f85627464448897bfa
tomorrowsville@gmail.com <tomorrowsville@gmail.com@f62a1bdd-e74c-edee-77cd-83dda710d615>
2011-04-09 00:47:19 +0000

    fixed indices

--- trunk/src/velbroad.f90
+++ trunk/src/velbroad.f90
@@ -13,13 +13,13 @@ SUBROUTINE VELBROAD(lambda,spec,sigma)
   REAL(SP), INTENT(in) :: sigma
   REAL(SP), DIMENSION(nspec) :: tspec,vel,func,gauss
   REAL(SP) :: c,cg,xmax
-  INTEGER :: i,j,m,il,ih, velocity=1
+  INTEGER :: i,j,il,ih,velocity=1,m=6
 
   !---------------------------------------------------------------!
   !---------------------------------------------------------------!
  
   c  = 2.998E5
-  cg = 1/sqrt(2*mypi)/sigma
+  cg = 1/SQRT(2*mypi)/sigma
 
   tspec = spec
   spec  = 0.0
@@ -27,7 +27,6 @@ SUBROUTINE VELBROAD(lambda,spec,sigma)
   !convolve at fixed sigma_velocity
   IF (velocity.EQ.1) THEN
 
-     m = 6
      DO i=1,nspec
         
         xmax = lambda(i)*(m*sigma/c+1)
@@ -36,10 +35,14 @@ SUBROUTINE VELBROAD(lambda,spec,sigma)
         
         vel(il:ih)  = (lambda(i)/lambda(il:ih)-1)*c
         func(il:ih) = tspec(il:ih) * cg*EXP(-vel(il:ih)**2/2./sigma**2)
-        DO j=il+1,ih
-           spec(i) = spec(i) + abs(vel(j)-vel(j-1))*(func(j)+func(j-1))/2.
-        ENDDO
-        
+
+        IF (il.EQ.ih) THEN
+           spec(i) = tspec(i)
+        ELSE
+           spec(i) = SUM( ABS(vel(il+1:ih)-vel(il:ih-1))*&
+                (func(il+1:ih)+func(il:ih-1))/2. )
+        ENDIF
+       
      ENDDO
 
   !convolve at fixed sigma_wavelength
