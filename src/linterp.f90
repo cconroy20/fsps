14461dc7865879088c44a00838534b01d868a3a0
tomorrowsville@gmail.com <tomorrowsville@gmail.com@f62a1bdd-e74c-edee-77cd-83dda710d615>
2011-12-19 15:48:26 +0000

    add linterp

---
+++ src/linterp.f90
@@ -0,0 +1,21 @@
+FUNCTION LINTERP(xin,yin,xout)
+
+  !routine to linearly interpolate a function yin(xin) at xout
+
+  USE sps_vars; USE nr, ONLY: locate
+  IMPLICIT NONE
+  REAL(SP), DIMENSION(:), INTENT(in) :: xin,yin
+  REAL(SP), INTENT(in)  :: xout
+  REAL(SP) :: linterp
+  INTEGER :: klo,n
+
+  !---------------------------------------------------------------!
+  !---------------------------------------------------------------!
+
+  n   = SIZE(xin)
+  klo = MAX(MIN(locate(xin,xout),n-1),1)
+
+  linterp = yin(klo) + (yin(klo+1)-yin(klo))*&
+       (xout-xin(klo))/(xin(klo+1)-xin(klo))
+
+END FUNCTION LINTERP
