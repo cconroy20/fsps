9b6a66a24289f1cd353b32b64a83763d5f3753bb
conroy.charlie <conroy.charlie@f62a1bdd-e74c-edee-77cd-83dda710d615>
2014-08-20 23:43:58 +0000

    added tpagb_norm_type parameter

--- trunk/src/mod_gb.f90
+++ trunk/src/mod_gb.f90
@@ -22,9 +22,13 @@ SUBROUTINE MOD_GB(zz,t,age,delt,dell,pagb,redgb,&
 
      !modify TP-AGB stars
      IF (phase(t,i).EQ.5.0) THEN
+
         !renormalize the Padova isochrones
-        !based on Conroy & Gunn (2010)
-        IF (isoc_type.EQ.'pdva') THEN
+        IF (isoc_type.EQ.'pdva'.AND.tpagb_norm_type.NE.0) THEN
+
+           !Conroy & Gunn (2010) normalization
+           IF (tpagb_norm_type.EQ.1) THEN
+
            IF (age(t).GT.age8.AND.age(t).LT.age91) THEN
               logl(t,i) = logl(t,i) - 1.0+(age(t)-8.)/1.5
               IF (LOG10(zlegend(zz)/zsol).LT.-0.25) THEN
@@ -35,9 +39,27 @@ SUBROUTINE MOD_GB(zz,t,age,delt,dell,pagb,redgb,&
                    MAX(MIN(0.4,-log10(zlegend(zz)/zsol)),0.2)
               logt(t,i) = logt(t,i) + 0.1 - MIN((age(t)-age91)/1.5,0.2)
            ENDIF
+
+           !Villaume, Conroy, Johnson (2014) normalization
+           ELSE IF (tpagb_norm_type.EQ.2) THEN
+
+              wght(i) = MAX(0.1,0.1+(age(t)-8.0)/3.0) * wght(i)
+
+              IF (age(t).GT.age8.AND.age(t).LT.age91) THEN
+                 IF (LOG10(zlegend(zz)/zsol).LT.-0.25) &
+                      logt(t,i) = logt(t,i) + 0.10
+              ELSE
+          !       logt(t,i) = logt(t,i) + 0.1 - MIN((age(t)-age91)/1.5,0.2)
+              ENDIF
+
+           ENDIF
+
         ENDIF
+
+        !add extra fudge factors to the default normalization
         logl(t,i) = logl(t,i) + dell
         logt(t,i) = logt(t,i) + delt
+
      ENDIF
 
      !modify post-AGB stars
