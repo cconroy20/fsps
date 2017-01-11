be8235e5ffaf578cd84194707241e828e892cd5e
Charlie Conroy <conroy.charlie@gmail.com>
2017-01-11 08:43:00 -0500

    added Pan-STARRS filters thanks to R. Gupta

--- src/sps_vars.f90
+++ src/sps_vars.f90
@@ -207,7 +207,7 @@ MODULE SPS_VARS
 
   !You must change the number of bands here if
   !filters are added to allfilters.dat
-  INTEGER, PARAMETER :: nbands=138
+  INTEGER, PARAMETER :: nbands=143
   !number of indices defined in allindices.dat
   INTEGER, PARAMETER :: nindx=30
   
